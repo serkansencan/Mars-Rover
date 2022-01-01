b70604687dbc504c999a2654759db054867543a6
Serkan Şencan <serkan.sencan@gmail.com>
2022-01-01 23:03:50 +0300

    Added redme and gitignore

--- MarsRovel/Program.cs
+++ MarsRovel/Program.cs
@@ -14,12 +14,12 @@ namespace MarsRover
             var rover1 = new Rover(0, 0, Orientations.N);
 
             var rover2 = new Rover(3, 3, Orientations.E);
-
             
             plateau.AddRovel(rover1);
             message = plateau.Process(rover1, "LMLMLMLMM");
             Console.WriteLine(message);
 
+            plateau.AddRovel(rover2);
             message = plateau.Process(rover2, "MMRMMRMRRM");
             Console.WriteLine(message);
         }
